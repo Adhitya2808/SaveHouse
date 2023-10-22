deaa8dcdf9ba9c2062ecb7c2020bd3c43763ed8f
Tohsaka <origamikertaslipat@gmail.com>
2023-10-22 21:01:12 +0700

    Update config.go

--- config/config.go
+++ config/config.go
@@ -2,10 +2,13 @@ package config
 
 import (
 	"SaveHouse/models"
-	"fmt"
-
-	"github.com/jinzhu/gorm"
+	"os"
+	"strconv"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/joho/godotenv"
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 var (
@@ -13,49 +16,63 @@ var (
 )
 
 
-func init() {
-	InitDB()
-	InitialMigration()
-}
-
 
 type Config struct {
-	DB_Username string
-	DB_Password string
-	DB_Port     string
-	DB_Host     string
-	DB_Name     string
+	SERVER_PORT int
+	DB_USER string
+	DB_PASS string
+	DB_HOST	string
+	DB_PORT	int
+	DB_NAME string
+	SIGN_KEY string
+	CLOUD_URL string
+	REFRESH_KEY string
 }
 
 
-func InitDB() {
 
+func LoadDBConfig() Config {
+	godotenv.Load(".env")
 
-	config := Config{
-		DB_Username: "root",
-		DB_Password: "",
-		DB_Port:     "3306",
-		DB_Host:     "localhost",
-		DB_Name:     "mini_project",
-	}
-
-
-	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
-		config.DB_Username,
-		config.DB_Password,
-		config.DB_Host,
-		config.DB_Port,
-		config.DB_Name,
-	)
-
-
-	var err error
-	DB, err = gorm.Open("mysql", connectionString)
+	DB_PORT, err := strconv.Atoi(os.Getenv("DB_PORT"))
+	SERVER_PORT, err := strconv.Atoi(os.Getenv("SERVER_PORT"))
 	if err != nil {
 		panic(err)
 	}
+
+	return Config {
+		DB_USER: os.Getenv("DB_USER"),
+		DB_PASS: os.Getenv("DB_PASS"),
+		DB_HOST: os.Getenv("DB_HOST"),
+		DB_PORT: DB_PORT,
+		DB_NAME: os.Getenv("DB_NAME"),
+		SERVER_PORT: SERVER_PORT,
+		SIGN_KEY: os.Getenv("SIGN_KEY"),
+		CLOUD_URL: os.Getenv("CLOUDINARY_URL"),
+		REFRESH_KEY: os.Getenv("REFRESH_KEY"),
+	}
 }
 
-func InitialMigration() {
-  	DB.AutoMigrate(&models.User{},&models.BarangIN{},&models.Barang{},&models.BarangOUT{})
+
+
+func InitDB() *gorm.DB{
+
+	config := LoadDBConfig()
+	
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", config.DB_USER, config.DB_PASS, config.DB_HOST, config.DB_PORT, config.DB_NAME)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	InitialMigration(db)
+
+	return db
+}
+
+
+func InitialMigration(db *gorm.DB) {
+  	db.AutoMigrate(&models.User{},&models.BarangIN{})
+	db.AutoMigrate(&models.Barang{},&models.BarangOUT{})
 }
