32726fe37a0a7e8c2156fc89ab30e62b16dc7514
Tohsaka <origamikertaslipat@gmail.com>
2023-10-22 20:54:45 +0700

    model user

---
+++ models/user.go
@@ -0,0 +1,11 @@
+package models
+
+type User struct {
+	ID 			uint64 `gorm:"primary_key;type:smallint" json:"id"`
+	Username	string `gorm:"type:varchar(255);not null" json:"username" form:"username"`
+	Password	string `gorm:"type:varchar(225);not null" json:"password" form:"password"`
+	Name		string `gorm:"type:varchar(255);not null" json:"name" form:"name"`
+	Role		string `gorm:"type:varchar(5);not null" json:"role" form:"role"`
+	Email		string `gorm:"type:varchar(255);not null" json:"email" form:"email"`
+}
+
