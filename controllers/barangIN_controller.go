30d02d62a46e3cbe6556fe0fdfa87b0820c30980
Tohsaka <origamikertaslipat@gmail.com>
2023-10-27 11:35:14 +0700

    refactor: automatic date format saat input barang in

--- controllers/barangIN_controller.go
+++ controllers/barangIN_controller.go
@@ -3,13 +3,17 @@ package controllers
 import (
 	"SaveHouse/config"
 	"SaveHouse/models"
-	"github.com/labstack/echo/v4"
 	"net/http"
+	"time"
+	"github.com/labstack/echo/v4"
 )
 
 func CreateBarangIN(c echo.Context) error {
 	// Parse JSON request body into a BarangIN models
 	var Barangmasuk models.BarangIN
+	if Barangmasuk.Transaction_IN.IsZero() {
+		Barangmasuk.Transaction_IN = time.Now()
+	}
 	if err := c.Bind(&Barangmasuk); err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
 	}
@@ -18,7 +22,6 @@ func CreateBarangIN(c echo.Context) error {
 	if err := config.DB.Create(&Barangmasuk).Error; err != nil {
 		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to create Barang_IN"})
 	}
-
 	// Return a JSON response with the created BarangIN
 	return c.JSON(http.StatusCreated, Barangmasuk)
 }
@@ -31,39 +34,4 @@ func GetAllBarangIN(c echo.Context) error {
 	}
 
 	return c.JSON(http.StatusOK, barangs)
-}
-
-func DeleteBarangIN(c echo.Context) error {
-	id := c.Param("trx_id")
-
-	var barang models.BarangIN
-	if err := config.DB.First(&barang, id).Error; err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to retrieve Barang_IN"})
-	}
-
-	if err := config.DB.Delete(&barang).Error; err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to delete Barang_IN"})
-	}
-
-	return c.JSON(http.StatusOK, map[string]string{"message": "Barang_IN successfully deleted"})
-}
-
-func UpdateBarangIN(c echo.Context) error {
-	id := c.Param("trx_id")
-
-	var barang models.BarangIN
-	if err := config.DB.First(&barang, id).Error; err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to retrieve room type"})
-	}
-
-	if err := c.Bind(&barang).Error; err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Cannot bind tipe kamar"})
-	}
-
-	if err := config.DB.Save(&barang).Error; err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to update room type"})
-	}
-
-	return c.JSON(http.StatusOK, barang)
-
 }
