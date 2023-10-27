e6d1463adc5212fd4b59169428f9fd3bea8a2064
Tohsaka <origamikertaslipat@gmail.com>
2023-10-27 16:18:00 +0700

    refactor: jwt for user and admin

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"fmt"
+	echojwt "github.com/labstack/echo-jwt/v4"
 	"net/http"
 	"os"
 	"time"
@@ -11,36 +12,34 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-type jwtCustomClaims struct {
-	ID   uint   `json:"id"`
-	Name string `json:"name"`
-	jwt.RegisteredClaims
+func JWTMiddleware() echo.MiddlewareFunc {
+	SecretKey := os.Getenv("JWT_KEY")
+	return echojwt.WithConfig(echojwt.Config{
+		SigningKey:    []byte(SecretKey),
+		SigningMethod: "HS256",
+	})
 }
 
-func CreateTokenUser(userId int, name string) string {
-	var payloadParser jwtCustomClaims
-	UserSecretKey := os.Getenv("USER_SECRET")
-
-	payloadParser.ID = uint(userId)
-	payloadParser.Name = name
-	payloadParser.ExpiresAt = jwt.NewNumericDate(time.Now().Add(time.Minute * 60))
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, payloadParser)
-	t, _ := token.SignedString([]byte(UserSecretKey))
-	return t
+func CreateToken(userId int, name, role string) (string, error) {
+	SecretKey := os.Getenv("JWT_KEY")
+	claims := jwt.MapClaims{}
+	claims["authorized"] = true
+	claims["ID"] = userId
+	claims["name"] = name
+	claims["role"] = role
+	claims["exp"] = time.Now().Add(time.Hour * 1).Unix() //Token expires after 1 hour
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(SecretKey))
 }
 
-func CreateTokenAdmin(userId int, name string) string {
-	var payloadParser jwtCustomClaims
-	AdminSecretKey := os.Getenv("ADMIN_SECRET")
-
-	payloadParser.ID = uint(userId)
-	payloadParser.Name = name
-	payloadParser.ExpiresAt = jwt.NewNumericDate(time.Now().Add(time.Minute * 60))
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, payloadParser)
-	t, _ := token.SignedString([]byte(AdminSecretKey))
-	return t
+func ExtractTokenUserRole(e echo.Context) string {
+	user := e.Get("user").(*jwt.Token)
+	if user.Valid {
+		claims := user.Claims.(jwt.MapClaims)
+		role := claims["role"].(string)
+		return role
+	}
+	return "user"
 }
 
 func HashPassword(password string) string {
