1f00a13dab623d7366bb5b9c8c119e42418c7a9a
Tohsaka <origamikertaslipat@gmail.com>
2023-10-28 19:45:02 +0700

    feat: Add feature for upload to cloudinary

--- controllers/barang_controller.go
+++ controllers/barang_controller.go
@@ -3,34 +3,73 @@ package controllers
 import (
 	"SaveHouse/config"
 	"SaveHouse/models"
+	"SaveHouse/service"
 	"github.com/labstack/echo/v4"
 	"net/http"
 )
 
 func CreateBarang(c echo.Context) error {
 	// Parse JSON request body into a Barang models
-	Barang := models.Barang{}
+	var Barang models.Barang
+
 	if err := c.Bind(&Barang); err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
 	}
 
 	// Create the new Barang in the database
+	fileheader := "photo"
+	Barang.Photo = service.CloudinaryUpload(c, fileheader)
 	if err := config.DB.Create(&Barang).Error; err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to create Barang_IN"})
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to create Barang Detail"})
 	}
 
-	// Return a JSON response with the created BarangIN
+	var BarangMasuk models.BarangIN
+	BarangMasuk.Transaction_IN = config.DB.NowFunc()
+	BarangMasuk.Trx_id = Barang.ID
+	if err := config.DB.Create(&BarangMasuk).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to create Barang_IN"})
+	}
+	if err := config.DB.Preload("Barangmasuk").Find(&Barang).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to retrieve Barang"})
+	}
+	models.BarangResponseConvert(Barang)
+
+	// Return a JSON response with the created Barang
 	return c.JSON(http.StatusCreated, Barang)
 }
 
-func GetAllBarang(c echo.Context) error {
-	var barangs []models.Barang
+func GetBarangByID(c echo.Context) error {
+	id := c.Param("id")
+	var barang []models.Barang
+	if err := config.DB.First(&barang, id).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to retrieve Barang"})
+	}
+	var responselist []models.BarangResponse
+	for _, Barang := range barang {
+		response := service.AllBarangsResponse(Barang)
+		responselist = append(responselist, response)
+	}
+	return c.JSON(http.StatusOK, responselist)
+}
 
-	if err := config.DB.Find(&barangs).Error; err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to retrieve room types"})
+func UpdateBarang(c echo.Context) error {
+	id := c.Param("id")
+
+	var barang models.Barang
+	if err := config.DB.First(&barang, id).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to retrieve BarangID"})
 	}
 
-	return c.JSON(http.StatusOK, barangs)
+	if err := c.Bind(&barang).Error; err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Cannot bind BarangID"})
+	}
+
+	if err := config.DB.Save(&barang).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to update BarangID"})
+	}
+
+	return c.JSON(http.StatusOK, barang)
+
 }
 
 func DeleteBarang(c echo.Context) error {
@@ -48,22 +87,12 @@ func DeleteBarang(c echo.Context) error {
 	return c.JSON(http.StatusOK, map[string]string{"message": "Barang_IN successfully deleted"})
 }
 
-func UpdateBarang(c echo.Context) error {
-	id := c.Param("id")
+func GetAllBarang(c echo.Context) error {
+	var barangs []models.Barang
 
-	var barang models.BarangIN
-	if err := config.DB.First(&barang, id).Error; err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to retrieve room type"})
+	if err := config.DB.Find(&barangs).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to retrieve room types"})
 	}
 
-	if err := c.Bind(&barang).Error; err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Cannot bind tipe kamar"})
-	}
-
-	if err := config.DB.Save(&barang).Error; err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to update room type"})
-	}
-
-	return c.JSON(http.StatusOK, barang)
-
+	return c.JSON(http.StatusOK, barangs)
 }
