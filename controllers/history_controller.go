cb35933968a4f5ab919173c7ebae98bfbf8ed6ad
Tohsaka <origamikertaslipat@gmail.com>
2023-10-30 23:32:19 +0700

    refactor: update barang

--- controllers/history_controller.go
+++ controllers/history_controller.go
@@ -1,1 +1,50 @@
 package controllers
+
+import (
+	"SaveHouse/config"
+	"SaveHouse/models"
+	"SaveHouse/service"
+	"github.com/labstack/echo/v4"
+	"net/http"
+	"strconv"
+)
+
+func GetAllHistory(c echo.Context) error {
+	var barang []models.Barang
+	if err := config.DB.Preload("Barangmasuk").Preload("Barangkeluar").Find(&barang).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to retrieve History Barang"})
+	}
+	var responselist []models.HistoryResponse
+	for _, barang := range barang {
+		response := service.AllHistoryResponse(barang)
+		responselist = append(responselist, response)
+	}
+	return c.JSON(http.StatusOK, responselist)
+}
+
+func Searching(c echo.Context) error {
+	var barang []models.Barang
+	search := c.QueryParam("search")
+	limit := c.QueryParam("limit")
+	page := c.QueryParam("page")
+
+	pageInt, _ := strconv.Atoi(page)
+	limitInt, _ := strconv.Atoi(limit)
+	offset := (pageInt - 1) * limitInt
+
+	if err := config.DB.Preload("Barangmasuk").Preload("Barangkeluar").Where("barang_name LIKE ?", "%"+search+"%").Offset(offset).Limit(limitInt).Find(&barang).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to retrieve Barang"})
+	}
+
+	if search == "" {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Failed to retrieve Barang"})
+	}
+
+	var responselist []models.HistoryResponse
+	for _, Barang := range barang {
+		response := service.AllHistoryResponse(Barang)
+		responselist = append(responselist, response)
+	}
+	return c.JSON(http.StatusOK, responselist)
+
+}
--- routes/API.go
+++ routes/API.go
@@ -1,1 +1,45 @@
 package routes
+
+import (
+	"SaveHouse/controllers"
+	"SaveHouse/middleware"
+	"os"
+
+	echojwt "github.com/labstack/echo-jwt/v4"
+	"github.com/labstack/echo/v4"
+)
+
+func New() *echo.Echo {
+	e := echo.New()
+
+	AdminSecretKey := os.Getenv("ADMIN_SECRET")
+
+	admin := e.Group("")
+	admin.Use(echojwt.JWT([]byte(AdminSecretKey)))
+
+	e.Use(middleware.NotFoundHandler)
+
+	//LOGIN USER DAN ADMIN
+	e.POST("/login", controllers.UserLogin)
+	e.POST("/login/admin", controllers.AdminLogin)
+
+	//BARANG
+	e.GET("/history", controllers.GetAllHistory)
+	e.GET("/history/", controllers.Searching)
+	e.POST("/in/barang", controllers.CreateBarang)
+	e.PUT("/in/barang/:id", controllers.UpdateBarang)
+	e.DELETE("/in/barang/:id", controllers.DeleteBarang)
+	e.POST("/out/barang", controllers.CreateBarangOUT)
+	e.GET("/barang", controllers.GetAllBarang)
+
+	//ADMIN
+	admin.GET("/akun", controllers.AllUser)
+	admin.GET("/akun/", controllers.UserbyID)
+	e.POST("/akun", controllers.Store)
+	admin.DELETE("/akun/:id", controllers.UserDelete)
+	e.PUT("/akun/:id", controllers.UserUpdate)
+
+	e.POST("/rekomendasi", controllers.GetRecommendation)
+
+	return e
+}
