8aa5ee0cd78071f6343c93a454a32571d8aec65e
Adhitya Ardhiansyah <104175819+Adhitya2808@users.noreply.github.com>
2023-10-30 21:50:10 +0700

    Update history_controller.go

--- controllers/history_controller.go
+++ controllers/history_controller.go
@@ -1,16 +1,1 @@
 package controllers
-
-import (
-	"SaveHouse/config"
-	"SaveHouse/models"
-	"github.com/labstack/echo/v4"
-	"net/http"
-)
-
-func GetAllHistory(c echo.Context) error {
-	var barangs []models.Barang
-	if err := config.DB.Preload("Barangmasuk").Preload("Barangkeluar").Find(&barangs).Error; err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to retrieve History Barang"})
-	}
-	return c.JSON(http.StatusOK, barangs)
-}
