8be06c234628efd9fb873b054bf8a63b3a86c730
Adhitya Ardhiansyah <104175819+Adhitya2808@users.noreply.github.com>
2023-10-30 21:55:39 +0700

    Update userResponse.go

--- models/web/userResponse.go
+++ models/web/userResponse.go
@@ -1,17 +1,1 @@
 package  web
-
-type UserResponse struct{
-	ID			int	   `json:"id" form:"id"`
-	Username	string `json:"username" form:"username"`
-	Name		string `json:"name" form:"name"`
-	Role		string `json:"role" form:"role"`
-	Email		string `json:"email" form:"email"`
-}
-
-type UserLoginResponse struct {
-	ID			int	   `json:"id"`
-	Username	string `json:"username"`
-	Role		string `json:"role"`
-	Token		string `json:"token"`
-	Password 	string `json:"password"`
-}
