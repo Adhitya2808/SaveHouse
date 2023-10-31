57f0422c6f08ac3a9838ff6254bcda88ab049b0d
Tohsaka <origamikertaslipat@gmail.com>
2023-10-31 07:54:47 +0700

    Squashed commit of the following:
    commit 898c40f8175a063c9bde354835e2b7ac99e94f7b
    Author: Adhitya Ardhiansyah <104175819+Adhitya2808@users.noreply.github.com>
    Date:   Mon Oct 30 21:08:18 2023 +0700
    
        Fix loginuser (#15)
    
        * Develops (#14)
    
        * model userResponse
    
        * model user
    
        * model base response
    
        * model user login response
    
        * model barang_out
    
        * model barang_in
    
        * model barang
    
        * model barang_inResponse
    
        * fitur user Without Login
    
        * delete
    
        * update env
    
        * Create go.sum
    
        * Create go.mod
    
        * Update config.go
    
        * Update config.go
    
        * Update user.go
    
        * Update user_controller.go
    
        * Update Config
    
        * update middleware
    
        * refactor: models
    
        * Add Utils
    
        * Update user_controller.go
    
        * Create admin_controller.go
    
        * Update .env
    
        * Update main.go
    
        * Update go.sum
    
        * Update go.mod
    
        * update controllers barang
    
        * Create barangIN_controller.go
    
        * feat(barang): added Barang History
    
        * Update .gitignore
    
        * refactor: model barang
    
        * Delete route.go
    
        * Delete cloud-upload.go
    
        * refactor: controller barang in
    
        * refactor: Login Admin Controller
    
        * refactor: automatic date format saat input barang in
    
        * feat: menambahkan fitur create barang_out
    
        * refactor: model barang in dan out
    
        * feat: routes (bug jwt)
    
        * Update .env
    
        * Update README.md
    
        * Update README.md
    
        * Update README.md
    
        * Update README.md
    
        * refactor: user controller
    
        * refactor: user controller
    
    commit 05c31606dbd76227e87d7cbf9e7ac1fdb5189edc
    Author: Adhitya Ardhiansyah <104175819+Adhitya2808@users.noreply.github.com>
    Date:   Mon Oct 30 20:40:35 2023 +0700
    
        Ai implement (#16)
    
        * feat: ai implement, searching and pagination
    
        * refactor: convert_response

--- controllers/barang_controller.go
+++ controllers/barang_controller.go
@@ -53,22 +53,32 @@ func GetBarangByID(c echo.Context) error {
 }
 
 func UpdateBarang(c echo.Context) error {
-	id := c.Param("id")
 
-	var barang models.Barang
-	if err := config.DB.First(&barang, id).Error; err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to retrieve BarangID"})
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid ID"})
 	}
-
-	if err := c.Bind(&barang).Error; err != nil {
+	var barang models.Barang
+	if err := c.Bind(&barang); err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Cannot bind BarangID"})
 	}
 
-	if err := config.DB.Save(&barang).Error; err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to update BarangID"})
+	var existingBarang models.Barang
+	result := config.DB.First(&existingBarang, id)
+	if result.Error != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to retrieve BarangID"})
 	}
+	existingBarang.Barang_Name = barang.Barang_Name
+	existingBarang.TipeGudang = barang.TipeGudang
+	existingBarang.Category = barang.Category
+	existingBarang.Description = barang.Description
+	existingBarang.Photo = barang.Photo
+	existingBarang.Quantity = barang.Quantity
+	config.DB.Model(&existingBarang).Updates(barang)
 
-	return c.JSON(http.StatusOK, barang)
+	response := utils.AllBarangsResponse(existingBarang)
+
+	return c.JSON(http.StatusOK, utils.SuccessResponse("Barang data successfully updated", response))
 
 }
 
@@ -86,6 +96,7 @@ func DeleteBarang(c echo.Context) error {
 
 	return c.JSON(http.StatusOK, map[string]string{"message": "Barang_IN successfully deleted"})
 }
+
 
 func GetAllBarang(c echo.Context) error {
 	var barangs []models.Barang
