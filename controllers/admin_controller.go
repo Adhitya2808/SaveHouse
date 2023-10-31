703a067000f4ce50fb490cab04c4d9a4d25d0b70
Adhitya Ardhiansyah <104175819+Adhitya2808@users.noreply.github.com>
2023-10-31 09:10:06 +0700

    Update admin_controller.go

--- controllers/admin_controller.go
+++ controllers/admin_controller.go
@@ -1,12 +1,12 @@
 package controllers
 
 import (
-	"SaveHouse/config"
-	"SaveHouse/middleware"
-	"SaveHouse/models"
-	"SaveHouse/models/web"
-	"SaveHouse/utils"
-	"SaveHouse/utils/res"
+	"app/config"
+	"app/middleware"
+	"app/models"
+	"app/models/web"
+	"app/utils"
+	"app/utils/res"
 	"github.com/labstack/echo/v4"
 	"net/http"
 	"strconv"
