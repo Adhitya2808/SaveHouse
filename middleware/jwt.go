10a2729490c04d36e6f8bd61421d7da8628578ec
Tohsaka <origamikertaslipat@gmail.com>
2023-11-03 18:46:20 +0700

    refactor:jwt

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -2,32 +2,46 @@ package middleware
 
 import (
 	"fmt"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/joho/godotenv"
+	echojwt "github.com/labstack/echo-jwt/v4"
+	"github.com/labstack/echo/v4"
+	"golang.org/x/crypto/bcrypt"
 	"net/http"
 	"os"
 	"time"
-
-	"github.com/golang-jwt/jwt/v5"
-	"github.com/labstack/echo/v4"
-	"golang.org/x/crypto/bcrypt"
 )
 
-type JwtCustomClaims struct {
-	ID   uint   `json:"id"`
-	Name string `json:"name"`
-	jwt.RegisteredClaims
+func JWTMiddleware() echo.MiddlewareFunc {
+	godotenv.Load(".env")
+	SecretKey := os.Getenv("ADMIN_SECRET")
+	return echojwt.WithConfig(echojwt.Config{
+		SigningKey:    []byte(SecretKey),
+		SigningMethod: "HS256",
+	})
 }
 
-func CreateTokenAdmin(userId int, name string) string {
-	var payloadParser JwtCustomClaims
-	AdminSecretKey := os.Getenv("ADMIN_SECRET")
+func CreateToken(userId int, username, role string) (string, error) {
+	godotenv.Load(".env")
+	SecretKey := os.Getenv("ADMIN_SECRET")
+	claims := jwt.MapClaims{}
+	claims["authorized"] = true
+	claims["userId"] = userId
+	claims["username"] = username
+	claims["role"] = role
+	claims["exp"] = time.Now().Add(time.Hour * 1).Unix() //Token expires after 1 hour
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(SecretKey))
+}
 
-	payloadParser.ID = uint(userId)
-	payloadParser.Name = name
-	payloadParser.ExpiresAt = jwt.NewNumericDate(time.Now().Add(time.Minute * 60))
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, payloadParser)
-	t, _ := token.SignedString([]byte(AdminSecretKey))
-	return t
+func ExtractTokenUserRole(e echo.Context) string {
+	user := e.Get("user").(*jwt.Token)
+	if user.Valid {
+		claims := user.Claims.(jwt.MapClaims)
+		role := claims["role"].(string)
+		return role
+	}
+	return "user"
 }
 
 func HashPassword(password string) string {
